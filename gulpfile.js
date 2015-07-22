945a0268d12a1e0b938dd1e11e81a005afc30c30
Eduardo Eidelwein Berlitz <eberlitz@gmail.com>
2015-07-22 15:42:16 -0300

    Node.js support

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require('gulp-uglify');
 var header = require('gulp-header');
 var pkg = require('./package.json');
+var concat = require('gulp-concat');
 
 var banner = ['/**',
     ' * <%= pkg.name %> - <%= pkg.description %>',
@@ -18,16 +19,31 @@ var banner = ['/**',
 ].join('\n');
 
 gulp.task('minify', function() {
-  return gulp.src('ngbearerauth.js')
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(header(banner, { pkg: pkg }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('.'));
+    return gulp.src([
+            'src/authservice.js',
+            'src/ng-bearer-auth.js'
+        ])
+        .pipe(concat('ng-bearer-auth.js'))
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('ngbearerauth.js', ['minify']);
+    gulp.watch([
+        'src/authservice.js',
+        'src/ng-bearer-auth.js'
+    ], ['minify']);
 });
 
 gulp.task('default', ['minify']);
